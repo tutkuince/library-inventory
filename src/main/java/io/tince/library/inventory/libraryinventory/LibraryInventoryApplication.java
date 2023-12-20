e2b7f0ac56f8eee7352c6e06ef82deac5710820e
Tutku Ince <tutku.ince@outlook.com>
2023-12-20 19:45:05 +0300

    initial commit

---
+++ src/main/java/io/tince/library/inventory/libraryinventory/LibraryInventoryApplication.java
@@ -0,0 +1,13 @@
+package io.tince.library.inventory.libraryinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/tince/library/inventory/libraryinventory/LibraryInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tince.library.inventory.libraryinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
