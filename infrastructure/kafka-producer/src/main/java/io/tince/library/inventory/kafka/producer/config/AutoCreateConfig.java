ad3918db53f022aeb850606fd982c45a1ba6dca0
Tutku Ince <tutku.ince@outlook.com>
2023-12-22 20:53:37 +0300

    uodate

---
+++ infrastructure/kafka-producer/src/main/java/io/tince/library/inventory/kafka/producer/config/AutoCreateConfig.java
@@ -0,0 +1,10 @@
+package io.tince.library.inventory.kafka.producer.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AutoCreateConfig {
+    
+}
